cf5268ac30a9f4f32a45ff9255d62e087deda5d7
Kiet <kietteik1@gmail.com>
2021-03-05 16:06:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { Card } from "./components/card/card";
+import { SearchBox } from "./components/search-box/search-box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: "",
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) => this.setState({ monsters: users }));
+    }
+
+    render() {
+        const { monsters, searchField } = this.state;
+        const filterMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+        return (
+            <div className="App">
+                <SearchBox
+                    handleChange={(a) => {
+                        this.setState(
+                            { searchField: a.target.value },
+                            (re) => {}
+                        );
+                    }}
+                />
+                <CardList name="Kox">
+                    {filterMonsters.map((monster) => (
+                        <Card key={monster.id} monster={monster} />
+                    ))}
+                </CardList>
+            </div>
+        );
+    }
 }
 
 export default App;
